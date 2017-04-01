c67fbc474e772c9a82dc1689bb5150809c336e95
J <shiyue@shiyuedeMacBook-Pro.local>
2017-04-01 11:07:44 +0800

    two day

--- Controller/BelleNavigationController.m
+++ Controller/BelleNavigationController.m
@@ -37,6 +37,29 @@
 
 }
 
+- (void)back {
+    [self popViewControllerAnimated:YES];
+
+}
+
+/**
+    当控制器，拿到导航控制器（需要是这个子类）， 进行压栈时都会调用这个方法
+    viewController 需要压栈的控制器
+    animate 动画
+ 
+ */
+- (void) pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    //拦截每一个pus的控制器进行统一设置
+    //过滤第一个跟控制器
+    if (self.childViewControllers.count > 0) {
+        //统一设置返回按钮
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_back_n"] style:0 target:self action:@selector(back)];
+    }
+    //  不能忘记push
+    [super pushViewController:viewController animated:animated];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
